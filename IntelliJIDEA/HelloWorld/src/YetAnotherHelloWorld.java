5d83c5c104847676492bf19b3067bd4c86192706
Besporydok <162829121+Besporydok@users.noreply.github.com>
2024-03-18 20:21:05 +0300

    03.18

---
+++ IntelliJIDEA/HelloWorld/src/YetAnotherHelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * A program that prints <code>Hello World!</code>
+ *
+ * @author Roman
+ * @version 1.0
+ */
+public class YetAnotherHelloWorld {
+    /**
+     * program entry point.
+     *
+     * @param cmdLineArgs command-line arguments
+     */
+    public static void main(String[] cmdLineArgs) {
+        System.out.println("Hello World");
+        // single line comment
+        /*
+        Multi-line comment
+         */
+
+    }
+}
---
+++ It's alive! It's alive!.java
@@ -0,0 +1,5 @@
+public class Main {
+ public static void main(String[] args) {
+  System.put.println("It's alive! It's alive!");
+  }
+}
